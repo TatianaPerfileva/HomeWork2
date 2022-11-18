9ae6a8a98fac887e7aa4cd14a9dc2ef05c593771
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-18 19:54:46 +0700

    add EXS 1

---
+++ EXS1/Program.cs
@@ -0,0 +1,5 @@
+﻿Random rand = new Random();
+int number;
+number = rand.Next(100, 1000);
+Console.WriteLine("Случайное трехзначное число:" + number);
+Console.WriteLine("Второе число:" + number/10%10);
