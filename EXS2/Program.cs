2d4f008c092090d1b5b80b7572a9315ebc96a894
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-18 21:02:18 +0700

    add EXS 2

---
+++ EXS2/Program.cs
@@ -0,0 +1,24 @@
+﻿Random rand = new Random();
+int number;
+number = rand.Next(0, 1000000);
+Console.WriteLine("Случайное число:" + number);
+if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+ else if (number < 1000)
+    {  
+        Console.WriteLine("Третья цифра:" + number%10);
+    } 
+ else if (number < 10000)
+    {
+        Console.WriteLine("Третья цифра:" + number%100/10);
+    }  
+ else if (number < 100000)
+    {
+        Console.WriteLine("Третья цифра:" + number%1000/100);
+    }      
+else if (number < 1000000)
+    {
+        Console.WriteLine("Третья цифра:" + number%10000/1000);
+    }
