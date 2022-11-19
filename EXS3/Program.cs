fce84801f1f86c7866bfb1ad0ca994b047b4ef19
TatianaPerfileva <byhgalter22@mail.ru>
2022-11-19 11:28:43 +0700

    add EXS 3

---
+++ EXS3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 6)  
+    {
+        Console.WriteLine("рабочий день");
+    }
+else if (number < 8)
+    {
+        Console.WriteLine("выходной день");
+    }
+else
+    {
+        Console.WriteLine("неверный ввод");
+    }
